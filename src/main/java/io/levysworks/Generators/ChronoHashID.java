aa39fd1fff58e9531b60b375b49a728e3563b375
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2025-08-20 00:14:10 +0200

    Initial commit

---
+++ src/main/java/io/levysworks/Generators/ChronoHashID.java
@@ -0,0 +1,57 @@
+package io.levysworks.Generators;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class ChronoHashID {
+    private static String getMacSegment() throws SocketException {
+        NetworkInterface ni = NetworkInterface.getNetworkInterfaces().nextElement();
+        byte[] mac = ni.getHardwareAddress();
+
+        String[] hexadecimal = new String[mac.length];
+        for (int i = 0; i < mac.length; i++) {
+            hexadecimal[i] = String.format("%02X", mac[i]);
+        }
+
+        return String.join("-", hexadecimal).substring(0, 7);
+    }
+
+    private static String hash(String value) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashBytes = md.digest(value.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hashBytes);
+    }
+
+    private static String getSecureString() {
+        String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+        String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+        String NUMBER = "0123456789";
+        String DATA_FOR_RANDOM_STRING = CHAR_LOWER + CHAR_UPPER + NUMBER;
+
+        SecureRandom sr = new SecureRandom();
+        StringBuilder sb = new StringBuilder(15);
+
+        for (int i = 0; i < 15; i++) {
+            int rndCharAt = sr.nextInt(DATA_FOR_RANDOM_STRING.length());
+            sb.append(DATA_FOR_RANDOM_STRING.charAt(rndCharAt));
+        }
+
+        return sb.toString();
+    }
+
+    public static String generateCHID() throws SocketException, NoSuchAlgorithmException {
+        String timeMillis = String.valueOf(System.currentTimeMillis());
+        String macSegment = getMacSegment();
+        String secureString = getSecureString();
+
+        String rawId = macSegment + secureString;
+        String hashedId = hash(rawId);
+
+        return timeMillis + hashedId;
+    }
+}
---
+++ src/main/java/io/levysworks/Mavlink/MissionFile.java
@@ -0,0 +1,54 @@
+package io.levysworks.Mavlink;
+
+public class MissionFile {
+    public final String fileType = "Plan";
+    public String groundStation;
+    public Mission mission;
+    public final int version = 1;
+
+    public MissionFile(Mission mission, String groundStation) {
+        this.mission = mission;
+        this.groundStation = groundStation;
+    }
+
+    public static class Mission {
+        public int cruiseSpeed;
+        public final int firmwareType = 12;
+        public final int globalPlanAltitudeMode = 1;
+        public int hoverSpeed;
+        public Item[] items;
+        public double[] plannedHomePosition;
+        public final int vehicleType = 2;
+        public final int version = 2;
+
+        public Mission(int cruiseSpeed, int hoverSpeed, Item[] items, double[] plannedHomePosition) {
+            this.cruiseSpeed = cruiseSpeed;
+            this.hoverSpeed = hoverSpeed;
+            this.items = items;
+            this.plannedHomePosition = plannedHomePosition;
+        }
+    }
+
+    public static class Item {
+        public Double AMSLAltAboveTerrain = null;
+        public int Altitude;
+        public int AltitudeMode;
+        public boolean autoContinue = true;
+        public int command;
+        public Integer doJumpId = null;
+        public int frame;
+        public Double[] params;
+        public final String type = "SimpleItem";
+
+        public Item(Double AMSLAltAboveTerrain, int altitude, int altitudeMode, boolean autoContinue, int command, Integer doJumpId, int frame, Double[] params) {
+            this.AMSLAltAboveTerrain = AMSLAltAboveTerrain;
+            this.Altitude = altitude;
+            this.AltitudeMode = altitudeMode;
+            this.autoContinue = autoContinue;
+            this.command = command;
+            this.doJumpId = doJumpId;
+            this.frame = frame;
+            this.params = params;
+        }
+    }
+}
