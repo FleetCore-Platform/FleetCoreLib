b935941a6a12e6b22672f173102965457ae457bf
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2025-09-04 00:05:15 +0200

    Added postgis jdbc package for future usage in algorithm.

--- src/main/java/io/levysworks/Algorithms/PolygonCoverageAlgorithm.java
+++ src/main/java/io/levysworks/Algorithms/PolygonCoverageAlgorithm.java
@@ -1,11 +1,12 @@
 //package io.levysworks.Algorithms;
 //
 //import io.levysworks.Mavlink.MissionFile;
+//import org.postgis.Geometry;
 //
 //import java.awt.*;
 //
 //public class PolygonCoverageAlgorithm {
-//    public static MissionFile calculateSinge(Polygon polygon) {
+//    public static MissionFile calculateSinge(Geometry polygon) {
 //
 //    }
 //}
