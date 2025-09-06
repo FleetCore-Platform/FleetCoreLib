ca0a98edd4999249c88843946eabcc477b7e1af8
Hagymasi <65444028+Levy-Y@users.noreply.github.com>
2025-09-06 14:52:46 +0200

    Moved MissionZipBuilder to this library from the backend

--- src/main/java/io/levysworks/Algorithms/PolygonCoverageAlgorithm.java
+++ src/main/java/io/levysworks/Algorithms/PolygonCoverageAlgorithm.java
@@ -1,12 +1,15 @@
-//package io.levysworks.Algorithms;
-//
-//import io.levysworks.Mavlink.MissionFile;
-//import org.postgis.Geometry;
-//
-//import java.awt.*;
-//
-//public class PolygonCoverageAlgorithm {
-//    public static MissionFile calculateSinge(Geometry polygon) {
-//
-//    }
-//}
+package io.levysworks.Algorithms;
+
+import io.levysworks.Mavlink.MissionFile;
+import org.postgis.Geometry;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class PolygonCoverageAlgorithm {
+    // TODO: Implement algorithm for calculating polygon survey path
+    public static File calculateSinge(Geometry polygon) throws IOException {
+        return File.createTempFile("temp", "zip");
+    }
+}
